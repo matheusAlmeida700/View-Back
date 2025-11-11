448493fb112f8adad63c60cc97a209cfec1271b4
matheusAlmeida700 <matheusauthentic8@gmail.com>
2025-11-11 15:58:21 -0300

    updated

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -25,6 +25,8 @@ export const universeSchema = Joi.object({
     width: Joi.number().required(),
     height: Joi.number().required(),
   }).required(),
+  shelfRows: Joi.number().default(0),
+  rowHeightM: Joi.number().default(0),
   color: Joi.string().default("hsl(0, 0%, 80%)"),
   opacity: Joi.number().default(0.75),
 });
@@ -33,24 +35,28 @@ export const categorySchema = Joi.object({
   id: Joi.string().required(),
   name: Joi.string().required(),
   universeId: Joi.string().required(),
+  row: Joi.number().default(0),
   offsetX: Joi.number().required(),
+  offsetY: Joi.number().default(0),
   width: Joi.number().required(),
-  minWidth: Joi.number().required(),
-  maxWidth: Joi.number().required(),
+  minWidth: Joi.number().default(0.5),
+  maxWidth: Joi.number().default(10),
   color: Joi.string().default("hsl(0, 0%, 80%)"),
   locked: Joi.boolean().default(false),
   opacity: Joi.number().default(0.9),
+  labelPosition: Joi.string().valid("top", "bottom", "center").default("top"),
+  meta: Joi.object().default({}),
 });
 
 export const pharmacySchema = Joi.object({
   userId: Joi.string().required(),
   name: Joi.string().required(),
-  storeSize: Joi.string().valid("P", "M", "G").default("G"),
+  storeSize: Joi.string().valid("P", "M", "G").required(),
   scalePixelsPerMeter: Joi.number().default(12),
   dimensions: Joi.object({
     width: Joi.number().required(),
     height: Joi.number().required(),
   }).required(),
-  universes: Joi.array().items(universeSchema).min(1),
-  categories: Joi.array().items(categorySchema).min(1),
+  universes: Joi.array().items(universeSchema).min(1).required(),
+  categories: Joi.array().items(categorySchema).min(1).required(),
 });
